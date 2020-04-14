607ca2138e3f63a43794b003f5b0fa964dceccac
l0tzi <lorenzmueller@hotmail.de>
2020-04-14 17:45:01 +0200

    bump influx2.0 and python3.0

--- src/dev.py
+++ src/dev.py
@@ -1,20 +1,20 @@
-from fronius_to_influx import FroniusToInflux
-from influxdb import InfluxDBClient
-from astral import Location
-
+#!/usr/bin/python3
+from fronius2influx import Fronius2Influx
+from influxdb_client import InfluxDBClient
+from astral import Observer
 import pytz
 
-
-client = InfluxDBClient(host='localhost', port=8087, username='grafana', password='grafana', ssl=False)
-client.switch_database('grafana')
-location = Location(('Poznan', 'Europe', 52.408078, 16.933618, 'Europe/Warsaw', 87))
-tz = pytz.timezone('Europe/Warsaw')
+city = Observer(50.118890, 10.675173, 15)
+client = InfluxDBClient.from_config_file('../conf/config.ini')
+bucket = 'grafana'
+tz = pytz.timezone("Europe/Berlin")
 endpoints = [
-    'http://172.30.1.11:5000/3PInverterData.json',
-    'http://172.30.1.11:5000/CommonInverterData.json',
-    'http://172.30.1.11:5000/MinMaxInverterData.json'
+    'http://10.0.0.210/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DataCollection=3PInverterData&DeviceId=1',
+    'http://10.0.0.210/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DataCollection=CommonInverterData&DeviceId=1',
+    'http://10.0.0.210/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DataCollection=MinMaxInverterData&DeviceId=1',
+    'http://10.0.0.210/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0',
+    'http://10.0.0.210/solar_api/v1/GetLoggerInfo.cgi'
 ]
-
-z = FroniusToInflux(client, location, endpoints, tz)
-z.IGNORE_SUN_DOWN = True
+z = Fronius2Influx(client, endpoints, bucket, city, tz)
+z.IGNORE_SUN_DOWN = False
 z.run()
--- src/fronius2influx.py
+++ src/fronius2influx.py
@@ -1,16 +1,15 @@
+#!/bin/bash
 # coding: utf-8
 import datetime
 import json
-from time import sleep
-from typing import Any, Dict, List, Type
+import time
 
 import pytz
-from astral import Location
-from influxdb import InfluxDBClient
+from astral import Observer, sun
 from requests import get
 from requests.exceptions import ConnectionError
-
-
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 class WrongFroniusData(Exception):
     pass
 
@@ -23,16 +22,22 @@ class DataCollectionError(Exception):
     pass
 
 
-class FroniusToInflux:
-    BACKOFF_INTERVAL = 3
+class Fronius2Influx(object):
     IGNORE_SUN_DOWN = False
+    BACKOFF_INTERVAL = 3
 
-    def __init__(self, client, location, endpoints, tz):
+    def __init__(self, client, endpoints, bucket, location, tz):
+        super().__init__()
         self.client = client
-        self.location = location
+        self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
         self.endpoints = endpoints
+        self.bucket = bucket
+        self.location = location
         self.tz = tz
-        self.data = None
+    
+    def on_exit(self):
+        self.write_api.__del__()
+        self.client.__del__()
 
     def get_float_or_zero(self, value):
         internal_data = None
@@ -42,32 +47,24 @@ class FroniusToInflux:
             raise WrongFroniusData('Response structure is not healthy.')
         return float(internal_data.get(value, {}).get('Value', 0))
 
+    def get_timestamp(self):
+        return self.data['Head']['Timestamp']
     def translate_response(self):
+        collection = None
         if 'DataCollection' in self.data['Head']['RequestArguments']:
             collection = self.data['Head']['RequestArguments']['DataCollection']
         elif 'DeviceClass' in self.data['Head']['RequestArguments']:
             collection = collection = self.data['Head']['RequestArguments']['DeviceClass']
         elif 'LoggerInfo' in self.data['Body'].keys():
             collection = 'LoggerInfo'
-        timestamp = self.data['Head']['Timestamp']
         if collection == 'CommonInverterData':
-            return [
-                {
-                    'measurement': 'DeviceStatus',
-                    'time': timestamp,
-                    'fields': {
+            return{
                         'ErrorCode': self.data['Body']['Data']['DeviceStatus']['ErrorCode'],
                         'LEDColor': self.data['Body']['Data']['DeviceStatus']['LEDColor'],
                         'LEDState': self.data['Body']['Data']['DeviceStatus']['LEDState'],
                         'MgmtTimerRemainingTime': self.data['Body']['Data']['DeviceStatus']['MgmtTimerRemainingTime'],
                         'StateToReset': self.data['Body']['Data']['DeviceStatus']['StateToReset'],
                         'StatusCode': self.data['Body']['Data']['DeviceStatus']['StatusCode'],
-                    }
-                },
-                {
-                    'measurement': collection,
-                    'time': timestamp,
-                    'fields': {
                         'FAC': self.get_float_or_zero('FAC'),
                         'IAC': self.get_float_or_zero('IAC'),
                         'IDC': self.get_float_or_zero('IDC'),
@@ -78,29 +75,17 @@ class FroniusToInflux:
                         'YEAR_ENERGY': self.get_float_or_zero('YEAR_ENERGY'),
                         'TOTAL_ENERGY': self.get_float_or_zero('TOTAL_ENERGY'),
                     }
-                }
-            ]
         elif collection == '3PInverterData':
-            return [
-                {
-                    'measurement': collection,
-                    'time': timestamp,
-                    'fields': {
+            return {
                         'IAC_L1': self.get_float_or_zero('IAC_L1'),
                         'IAC_L2': self.get_float_or_zero('IAC_L2'),
                         'IAC_L3': self.get_float_or_zero('IAC_L3'),
                         'UAC_L1': self.get_float_or_zero('UAC_L1'),
                         'UAC_L2': self.get_float_or_zero('UAC_L2'),
                         'UAC_L3': self.get_float_or_zero('UAC_L3'),
-                    }
                 }
-            ]
         elif collection == 'MinMaxInverterData':
-            return [
-                {
-                    'measurement': collection,
-                    'time': timestamp,
-                    'fields': {
+            return {
                         'DAY_PMAX': self.get_float_or_zero('DAY_PMAX'),
                         'DAY_UACMAX': self.get_float_or_zero('DAY_UACMAX'),
                         'DAY_UDCMAX': self.get_float_or_zero('DAY_UDCMAX'),
@@ -111,74 +96,67 @@ class FroniusToInflux:
                         'TOTAL_UACMAX': self.get_float_or_zero('TOTAL_UACMAX'),
                         'TOTAL_UDCMAX': self.get_float_or_zero('TOTAL_UDCMAX'),
                     }
-                }
-            ]
         elif collection == 'Meter':
-            return [
-                {
-                    'measurement': collection,
-                    'time': timestamp,
-                    'fields': {
+            return {
                         'PowerReal_P_Phase_1': self.data["Body"]["Data"]["PowerReal_P_Phase_1"],
                         'PowerReal_P_Phase_2': self.data["Body"]["Data"]["PowerReal_P_Phase_2"],
                         'PowerReal_P_Phase_3': self.data["Body"]["Data"]["PowerReal_P_Phase_3"],
-                        'CurrentConsumption': float(self.data["Body"]["Data"]["PowerReal_P_Sum"]) 
+                        'CurrentConsumption': (float(self.data["Body"]["Data"]["PowerReal_P_Sum"] ))
                     }
-                }
-            ]
         elif collection == 'LoggerInfo':
             keyname = 'LoggerInfo'
-            return [
-                {
-                    'measurement': collection,
-                    'time': timestamp,
-                    'fields': {
+            return {
                         'CO2Factor': self.data['Body'][keyname]['CO2Factor'],
                         'CashFactor': self.data['Body'][keyname]['CashFactor'],
                         'CashFactor': self.data['Body'][keyname]['CashFactor'],
                         'HWVersion': self.data['Body'][keyname]['HWVersion'],
                         'SWVersion': self.data['Body'][keyname]['SWVersion']
                     }
-                }
-            ]
         else:
             raise DataCollectionError("Unknown data collection type.")
 
-
     def sun_is_shining(self):
-        sun = self.location.sun()
-        if not self.IGNORE_SUN_DOWN and not sun['sunrise'] < datetime.datetime.now(tz=self.tz) < sun['sunset']:
+        sunrise = sun.sunrise(self.location, datetime.datetime.now(), self.tz)
+        sunset = sun.sunset(self.location, datetime.datetime.now(), self.tz)
+        if not self.IGNORE_SUN_DOWN and not sunrise < datetime.datetime.now(tz = self.tz) < sunset:
             raise SunIsDown
         return None
 
     def run(self):
         try:
-            while True:
+            while(True):
                 try:
                     self.sun_is_shining()
-                    collected_data = []
+                    collected_data = {}
                     for url in self.endpoints:
-                        response = get(url)
-                        self.data = response.json()
-                        collected_data.extend(self.translate_response())
-                        sleep(self.BACKOFF_INTERVAL)
-                    self.client.write_points(collected_data)
-                    print('Data written')
-                    sleep(self.BACKOFF_INTERVAL)
+                        #print(url)
+                        self.data = get(url).json()
+                        timestamp = self.data['Head']['Timestamp']
+                        collected_data.update(self.translate_response())
+                    #create output -> Timestamp + Fields
+                    export_struct = {
+                        'measurement': 'fronius',
+                        'time': timestamp,
+                        'fields': collected_data,
+                        'tags': {'location' : 'fronius'}
+                    }
+                    #write2Influx2
+                    self.write_api.write(bucket= self.bucket, record= Point.from_dict(export_struct))
+                    print("Write successful - sleeping 30 sec")
+                    time.sleep(30)
                 except SunIsDown:
-                    sleep(60)
+                    time.sleep(60)
                     print('Waited 60 seconds for sunrise')
                 except ConnectionError as e:
                     print("Exception: {}".format(e))
                     print("Waiting for connection...")
-                    sleep(10)
+                    time.sleep(10)
                     print('Waited 10 seconds for connection')
                 except KeyError:
                     raise WrongFroniusData('Response structure is not healthy')
                 except Exception as e:
                     self.data = {}
-                    sleep(10)
+                    time.sleep(10)
                     print("Exception: {}".format(e))
-
         except KeyboardInterrupt:
             print("Finishing. Goodbye!")
